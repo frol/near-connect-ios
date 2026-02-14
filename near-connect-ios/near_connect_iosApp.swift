deb200a7d82d209380e2ef74942b61ce7496500f
Vlad Frolov <frolvlad@gmail.com>
2026-02-14 13:34:42 +0100

    Initial Commit

---
+++ near-connect-ios/near_connect_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  near_connect_iosApp.swift
+//  near-connect-ios
+//
+//  Created by vm on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct near_connect_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
